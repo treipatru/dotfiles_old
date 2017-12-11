24c25fcf2fbf7fcbaf2f9c1507b871ccfaadfa5e
treipatru <andrei@planet34.org>
2017-12-11 13:27:34 +0100

    Renamed file

---
+++ scripts/lenovo-key-remap.sh
@@ -0,0 +1,6 @@
+setxkbmap -option caps:none;
+xmodmap -e "keycode 112 = Home" ;
+xmodmap -e "keycode 117 = End" ;
+xmodmap -e "keycode 110 = Prior" ;
+xmodmap -e "keycode 115 = Next" ;
+xmodmap -pke >~/.Xmodmap
