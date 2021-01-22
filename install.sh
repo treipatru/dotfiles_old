47a48125496b022f613bbd3e39286e2db46f80f6
Andrei Mocanca <andrei.mocanca@keeneye.tech>
2021-01-22 02:20:11 +0100

    Finish new version of the installer

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # 
 #   (          ) (       (         
 #   )\ )    ( /( )\ ) (  )\  (     
@@ -10,36 +9,70 @@
 # \__,_\___/\__| |_|  |_|_\___/__/ 
 # 
 
-console_log () {
-  printf "\n >>>>> ${1} \n"
+
+######## Defs
+GitSourceUrl=https://github.com/treipatru/dotfiles.git
+GitDestinationPath=~/repos/dotfiles
+GitDestinationRoot=${GitDestinationPath%/*}
+SourceRootConfig=tst
+
+# Logging
+ConsoleLog () { printf "\n ░░▎${1}\n"; }
+
+# Abort install
+ConsoleExit () { ConsoleLog "$@"; ConsoleLog "Aborting install"; exit 0; }
+
+# Recursively link all files from one location to another
+RecursiveLink () {
+    local source=$1/**.*
+
+    for node in $source
+    do
+        echo $node
+        # if [[ -d $node ]]; then RecursiveLink $node; fi
+        # if [[ -f $node ]]; then echo $node; fi
+    done
 }
 
-console_exit () {
-  printf "\n >>>>> ${1}"
-  printf "\n >>>>> Aborting install"
-  exit 0
-}
 
-# Exit if no git
-if ! hash git 2>/dev/null; then console_exit "Install Git to continue"; fi
+######## Check
+# Git
+if ! hash git 2>/dev/null; then ConsoleExit "Install Git to continue"; fi
 
-# Exit if dotfiles already present
- [[ ! -d ~/repos/dotfiles ]] && console_exit "/repos/dotfiles already exists. Update via Git."
+# Repository already present
+# [[ -d $GitDestinationPath ]] && ConsoleExit "${GitDestinationPath} already exists."
 
-#
-git clone "https://github.com/treipatru/dotfiles.git"
 
-# Make some folders if they don't exist
-mkdir -p {~/.config,~/repos,~/scripts,~/.ssh,~/.wsh}
+######## Run
+# Make any missing dirs
+# mkdir -p {~/.config,~/scripts,~/.ssh,$GitDestinationRoot}
 
-# Touch empty config files
-touch ~/{.zsh.local,.aliases.local}
+# Clone repo
+# git -C $GitDestinationRoot clone $GitSourceUrl 
+
+# Touch additional config files
+# touch ~/{.zsh.local,.aliases.local}
 
 # Create example config SSH
-[[ ! -f ~/.ssh/config ]] && echo \
-"host example.com
-	HostName example.com
-	Port 22
-	IdentityFile ~/.ssh/id_rsa
-	User login"\
->> ~/.ssh/config
+# [[ ! -f ~/.ssh/config ]] && echo \
+# "host example.com
+# 	HostName example.com
+# 	Port 22
+# 	IdentityFile ~/.ssh/id_rsa
+# 	User login"\
+# >> ~/.ssh/config
+
+# Link config files to ~
+pth=${GitDestinationPath}/${SourceRootConfig}/
+find $pth -mindepth 1 -type d| while read lnSource; do
+    lnDest=~/${lnSource#"$pth"}
+    echo "link dest: " $lnDest
+    # ln -sf $lnSource $lnDest
+    # [[ -f $lnDest ]] && ln -sf $lnDest
+done
+
+# find $pth -type f| while read lnSource; do
+#     lnDest=~/${lnSource#"$pth"}
+#     echo "link dest: " $lnDest
+#     ln -sf $lnSource $lnDest
+# done
