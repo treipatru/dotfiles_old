054bc75dd9fccd399609753a7233220e00c387bd
treipatru <andrei@planet34.org>
2021-10-31 15:50:00 +0100

    add lsp config

---
+++ files/.config/nvim/lua/plugins/lang-server-config.lua
@@ -0,0 +1,23 @@
+local lspcfg = require('lspconfig')
+
+-- Coq setttings must be defined before require
+vim.g.coq_settings = {
+  ['auto_start'] = 'shut-up',
+  ['keymap.recommended'] = false,
+}
+local coq = require "coq"
+
+-- Servers definitions
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local servers = {
+    'tsserver',
+    'sumneko_lua',
+}
+for _, lsp in ipairs(servers) do
+  lspcfg[lsp].setup(coq.lsp_ensure_capabilities(capabilities))
+end
+
+-- TypeScript
+lspcfg.tsserver.setup{}
+-- LUA
+require('plugins.lang-server-lua')
