12e82ff2a826baed384ee24bb81752e030a60431
treipatru <andrei@planet34.org>
2021-10-31 18:54:33 +0100

    cleanup init

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -1,35 +1,20 @@
-vim.g.mapleader = ' '        -- assign leader key
-require('settings')          -- base settings
+vim.g.mapleader = ' '                       -- assign leader key
+require'settings'                           -- base settings
 
-require('plugins')           -- packer plugins
-vim.cmd [[packadd packer.nvim]]  -- init packer
+require'plugins'                            -- packer plugins
+vim.cmd [[packadd packer.nvim]]             -- init packer
 
-require('plugins.gitsigns')  -- git gutter
--- require('plugins.nvim-cmp')  -- autocomplete
-require('plugins.telescope') -- telescope cfg
-require('plugins.treesitter')  -- autocomplete
-require('plugins.webdev-icons') -- icons
-require('plugins.staline') -- status line
-require('plugins.shade') -- dim inactive window
+require'colorizer'.setup()                  -- colorize color strings
+require'plugins.autopairs'                  -- autopairs config
+require'plugins.gitsigns'                   -- git gutter
+require'plugins.lang-server-config'         -- lsp settings
+require'plugins.shade'                      -- dim inactive window
+require'plugins.staline'                    -- status line
+require'plugins.telescope'                  -- telescope cfg
+require'plugins.treesitter'                 -- autocomplete
+require'plugins.webdev-icons'               -- icons
 
-require('utils')             -- autocmds
-require('keymaps')           -- keys
+require'utils'                              -- autocmds
+require'keymaps'                            -- keys
 
-vim.cmd [[colorscheme nordfox]]  -- set theme
-
-vim.g.coq_settings = {
-  auto_start = 'shut-up',
-}
-
-local lsp = require('lspconfig')
-local coq = require "coq"
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'tsserver' }
-for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup(coq.lsp_ensure_capabilities(capabilities))
-end
-
-require'lspconfig'.tsserver.setup{}
-require'colorizer'.setup()
+vim.cmd [[colorscheme nordfox]]             -- set theme
