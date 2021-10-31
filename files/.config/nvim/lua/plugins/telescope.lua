b3c9876782356ea2d3956380711d7fda8abe6f9b
treipatru <andrei@planet34.org>
2021-10-31 21:31:08 +0100

    update telescope cfg

--- files/.config/nvim/lua/plugins/telescope.lua
+++ files/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,34 @@
 local actions = require('telescope.actions')
+
 require('telescope').setup{
   defaults = {
     mappings = {
       i = {
-        ["<esc>"] = actions.close
+        ["<esc>"] = actions.close,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-s>"] = actions.toggle_selection,
+        ["<C-q>"] = actions.smart_send_to_qflist,
       },
     },
-  }
+  },
+  pickers = {
+      buffers = {
+          theme = "ivy",
+      },
+      find_files = {
+          previewer = false,
+          theme = "ivy",
+      },
+      git_files = {
+          previewer = false,
+          theme = "ivy",
+      },
+      grep_string = {
+          theme = "ivy",
+      },
+      live_grep = {
+          theme = "ivy",
+      },
+  },
 }
