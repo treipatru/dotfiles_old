8beae7f9b5db5592996f57a2a16f756bd39ede08
treipatru <andrei@planet34.org>
2021-10-31 17:14:57 +0100

    add autotag

--- files/.config/nvim/lua/plugins.lua
+++ files/.config/nvim/lua/plugins.lua
@@ -66,6 +66,7 @@ return require('packer').startup(function(use)
   use 'tpope/vim-commentary'
   use 'tpope/vim-fugitive'
   use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
