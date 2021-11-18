5a390f589ee52ab306b145cfe1257f28dd3bc3ff
treipatru <andrei@planet34.org>
2021-11-18 01:20:34 +0100

    add custom telescope pickers

---
+++ files/.config/nvim/lua/plugins/telescope-custom.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+-- Switch to find files if cwd is not repo
+M.project_files = function()
+    local opts = {}
+    local ok = pcall(require'telescope.builtin'.git_files, opts)
+    if not ok then require'telescope.builtin'.find_files(opts) end
+end
+
+-- Search for snippets
+M.snippets = function()
+    local opts = {
+        search_dirs = {"~/snippets"},
+    }
+
+    require'telescope.builtin'.find_files(opts)
+end
+
+-- File browser in current buffer directory
+M.browse_buffer_folder  = function()
+    local opts = {
+        cwd = require("telescope.utils").buffer_dir()
+    }
+
+    require'telescope.builtin'.file_browser(opts)
+end
+
+-- Prompt for word to grep
+function M.grep_prompt()
+  require("telescope.builtin").grep_string {
+    path_display = {
+        "shorten",                     -- shorten paths to max 4 chars
+        shorten = 4,
+    },
+    search = vim.fn.input "Rg ",
+  }
+end
+
+return M
