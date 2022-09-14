d815d3c355e700fa1163c94c432d2fdd8becd85c
treipatru <andrei@planet34.org>
2022-09-14 10:53:27 +0200

    update treesitter cfg to latest version

--- files/.config/nvim/lua/plugins/treesitter.lua
+++ files/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,5 @@
 require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
   ensure_installed = {
       "bash",
       "css",
@@ -18,12 +19,32 @@ require'nvim-treesitter.configs'.setup {
       "tsx",
       "typescript",
       "vim",
+      "vue",
       "yaml",
-  }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = { }, -- List of parsers to ignore installing
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
   highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = { },  -- list of language that will be disabled
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { },
+
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -44,7 +65,6 @@ require'nvim-treesitter.configs'.setup {
       enable = true,
       keymaps = {
           ['<cr>'] = 'textsubjects-smart',
-          -- [';'] = 'textsubjects-container-outer',
       }
   },
 }
