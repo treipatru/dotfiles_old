f91e69f3479bdd70cd70004fc7a6b0a87218589b
treipatru <andrei@planet34.org>
2022-10-22 13:49:25 +0200

    update theme

--- files/.config/nvim/lua/plugins/theme.lua
+++ files/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,12 @@
-vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
 require('catppuccin').setup({
-    transparent_background = true,
+    transparent_background = false,
     term_colors = false,
     dim_inactive = {
-        enabled = false,
+        enabled = true,
         shade = "dark",
-        percentage = 0.15,
+        percentage = 0.2,
     },
     styles = {
         comments = { "italic" },
@@ -24,9 +24,29 @@ require('catppuccin').setup({
     },
     integrations = {
         gitsigns = true,
+        gitgutter = true,
         nvimtree = true,
         telescope = true,
         treesitter = true,
+        ts_rainbow = true,
+        which_key = true,
+        cmp = true,
+        mason = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+			},
+		},
         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
     },
     color_overrides = {},
