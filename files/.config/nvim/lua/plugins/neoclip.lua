1e0442ee14936394ea674f1a2f13f175c7fe8e62
treipatru <andrei@planet34.org>
2022-11-21 21:29:40 +0100

    add neoclip config

--- files/.config/nvim/lua/plugins/neoclip.lua
+++ files/.config/nvim/lua/plugins/neoclip.lua
@@ -1,1 +1,22 @@
-require('neoclip').setup()
+require('neoclip').setup({
+    filter = nil,
+    preview = true,
+    default_register = '"',
+    default_register_macros = 'q',
+    content_spec_column = false,
+    keys = {
+        telescope = {
+            i = {
+                select = '<c-x>',
+                paste = '<cr>',
+                paste_behind = '<c-z>',
+            },
+        },
+        fzf = {
+            select = 'default',
+            paste = 'ctrl-p',
+            paste_behind = 'ctrl-k',
+            custom = {},
+        },
+    },
+})
