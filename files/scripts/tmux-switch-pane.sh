c9ffb4efb4f06d4ba9a4d21bbad8d8365cebf37c
treipatru <andrei@planet34.org>
2023-09-02 00:27:37 +0200

    better tmux window jumper

--- files/scripts/tmux-switch-pane.sh
+++ files/scripts/tmux-switch-pane.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # customizable
-LIST_DATA="#{window_name} #{pane_title} #{pane_current_path} #{pane_current_command}"
+LIST_DATA="#{session_name} | #{window_name} | #{pane_current_path} > #{pane_current_command}"
 FZF_COMMAND="fzf-tmux -p --delimiter=: --with-nth 4 --color=hl:2"
 
 # do not change
@@ -12,4 +12,4 @@ LINE=$(tmux list-panes -a -F "$TARGET_SPEC $LIST_DATA" | $FZF_COMMAND) || exit 0
 # split the result
 args=(${LINE//:/ })
 # activate session/window/pane
-tmux select-pane -t ${args[2]} && tmux select-window -t ${args[1]} && tmux switch-client -t ${args[0]}
+tmux select-pane -t "${args[2]}" && tmux select-window -t "${args[1]}" && tmux switch-client -t "${args[0]}"
